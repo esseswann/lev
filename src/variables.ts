2dc0e4bb5d5239d6f8a5b91975154274e9e9af76
jsus <esseswann@gmail.com>
2023-05-05 19:23:37 +0300

    Some default value handling

--- src/variables.ts
+++ src/variables.ts
@@ -1,14 +1,47 @@
-import { Kind, VariableDefinitionNode } from 'graphql'
+import { ConstValueNode, Kind, VariableDefinitionNode } from 'graphql'
 
 const getVariable = (variable: VariableDefinitionNode) => {
-  if (
-    variable.type.kind !== Kind.NAMED_TYPE ||
-    variable.defaultValue?.kind === Kind.LIST ||
-    variable.defaultValue?.kind === Kind.OBJECT ||
-    variable.defaultValue?.kind === Kind.NULL
-  )
-    throw new Error('Only named types are supported')
-  return `$${variable.variable.name.value} = Cast('${variable.defaultValue?.value}' as ${variable.type.name.value});`
+  const handler = getHandler(variable.type)
+  return handler(variable)
 }
+
+const getHandler = (type: VariableDefinitionNode['type']): Handler => {
+  switch (type.kind) {
+    case Kind.NAMED_TYPE:
+      return handleNamedType
+    case Kind.NON_NULL_TYPE:
+      return getHandler(type)
+    case Kind.LIST_TYPE:
+      throw new Error('List type non implemented')
+  }
+}
+
+const handleNamedType: Handler = (variable) => {
+  if (!variable.defaultValue)
+    throw new Error('Only variables with default value are supported')
+  let result = `$${variable.variable.name.value} = ${handleDefaultValue(
+    variable.defaultValue
+  )}`
+  return result
+}
+
+const handleDefaultValue = (value: ConstValueNode): string => {
+  switch (value.kind) {
+    case Kind.NULL:
+      return 'null'
+    case Kind.INT:
+    case Kind.FLOAT:
+    case Kind.STRING:
+      return value.value
+    case Kind.BOOLEAN:
+      return value.value.toString()
+    case Kind.ENUM:
+    case Kind.LIST:
+    case Kind.OBJECT:
+      throw new Error('Unsupported type')
+  }
+}
+
+type Handler = (variable: VariableDefinitionNode) => string
 
 export default getVariable
