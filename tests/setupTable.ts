d3766ecb08a9d278622f295e96bce7aaa104677e
jsus <esseswann@yandex.ru>
2023-08-06 16:42:21 +0300

    Setup table example added

--- tests/setupTable.ts
+++ tests/setupTable.ts
@@ -1,4 +1,13 @@
-const processTable = (folder: string) => {
+import path from 'path'
+import { Driver } from 'ydb-sdk'
+import { readFile } from 'fs/promises'
+
+export const processTable = async (database: Driver, folder: string) => {
+  const tablePath = path.join(folder, 'table.sql')
+  const tableQuery = await readFile(tablePath, 'utf-8')
+  await database.tableClient.withSessionRetry((session) =>
+    session.executeQuery(tableQuery))
+  const dataPath = path.join(folder, 'data.csv')
   // TODO create schema from sql
   // TODO add data from csv
 }
