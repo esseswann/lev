20210307fe529c5b1270f9c35d422d5ddcc0dd93
jsus <esseswann@yandex.ru>
2023-08-06 17:53:16 +0300

    Better convertRows helper

--- tests/basic/tables/user/index.ts
+++ tests/basic/tables/user/index.ts
@@ -1,20 +1,22 @@
 import { Column, Session, TableDescription, TypedValues, Types, Ydb } from "ydb-sdk";
 import data from './data.json'
 
+const table = new TableDescription()
+  .withColumn(new Column('id', Types.UINT64))
+  .withColumn(new Column('name', Types.UTF8))
+  .withPrimaryKey('id')
+
 export const up = (session: Session, path: string) =>
-  session.createTable(path, new TableDescription()
-    .withColumn(new Column('id', Types.UINT64))
-    .withColumn(new Column('name', Types.UTF8))
-    .withPrimaryKey('id'))
+  session.createTable(path, table)
 
 export const seed = (session: Session, path: string) =>
   session.bulkUpsert(path, convertRows(data))
 
 const convertRows = (data: unknown[]): Ydb.TypedValue =>
-  TypedValues.list(Types.struct({
-    id: Types.UINT64,
-    name: Types.UTF8
-  }), data) as Ydb.TypedValue
+  TypedValues.list(Types.struct(table.columns.reduce((result, column) => {
+    result[column.name] = column.type
+    return result
+  }, {})), data) as Ydb.TypedValue
 
 export const down = (session: Session, path: string) =>
   session.describeTable(path)
