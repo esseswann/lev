aabf281eb6bebe30f99914a93ca12b420633f1a3
Sergei Popinevskii <gurza000@gmail.com>
2023-08-06 19:14:01 +0300

    fix user table description

--- tests/basic/tables/user/index.ts
+++ tests/basic/tables/user/index.ts
@@ -2,7 +2,7 @@ import { Column, TableDescription, Types } from 'ydb-sdk'
 
 const table = new TableDescription()
   .withColumn(new Column('id', Types.UINT64))
-  .withColumn(new Column('name', Types.UTF8))
+  .withColumn(new Column('name', Types.optional(Types.UTF8)))
   .withPrimaryKey('id')
 
 export default table
