bf05936dfd956aba471f1de8cfea3f3902d84f4a
Sergei Popinevskii <gurza000@gmail.com>
2023-08-06 20:06:54 +0300

    test table seed operation

--- tests/basic/index.test.ts
+++ tests/basic/index.test.ts
@@ -1,20 +1,43 @@
+import { TypedData } from 'ydb-sdk'
 import database from '../database'
+import setupTable from '../setupTable'
+import accessTable from './tables/access'
+import userTable from './tables/user'
+import userData from './tables/user/data'
 
-// TODO an example of structure
+const dirname = __dirname.split('/').pop()
 
 beforeAll(async () => {
-  await database.ready()
+  const timeout = 10000
+  await database.ready(timeout)
+  await database.tableClient.withSessionRetry(async (session) =>
+    Promise.all([
+      setupTable(session, `${dirname}/user`, userTable, userData),
+      setupTable(session, `${dirname}/access`, accessTable)
+    ])
+  )
 })
 
 afterAll(async () => {
-  await database.close()
+  await database.tableClient.withSessionRetry(async (session) =>
+    Promise.all([
+      session.dropTable(`${dirname}/user`),
+      session.dropTable(`${dirname}/access`)
+    ])
+  )
+  await database.schemeClient.removeDirectory(dirname!)
 })
 
 describe('my database tests', () => {
   test('should insert data into the database', async () => {
-    const collection = database.collection('myCollection')
-    const data = { name: 'John Doe' }
-    const result = await collection.insertOne(data)
-    expect(result.insertedCount).toBe(1)
+    const query = `SELECT * FROM \`${dirname}/user\` where id = 1`
+    const result = await database.tableClient.withSessionRetry(
+      async (session) => {
+        const result = await session.executeQuery(query)
+        const resultSet = result.resultSets[0]
+        return TypedData.createNativeObjects(resultSet)
+      }
+    )
+    expect(result[0]['id']).toBe(1)
   })
 })
