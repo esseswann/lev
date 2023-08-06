70d4d5f28b73ce8671744aab00a2c3e052feac8e
Sergei Popinevskii <gurza000@gmail.com>
2023-08-06 19:13:39 +0300

    describe access table

---
+++ tests/basic/tables/access/index.ts
@@ -0,0 +1,8 @@
+import { Column, TableDescription, Types } from 'ydb-sdk'
+
+const table = new TableDescription()
+  .withColumn(new Column('id', Types.UINT64))
+  .withColumn(new Column('name', Types.optional(Types.UTF8)))
+  .withPrimaryKey('id')
+
+export default table
