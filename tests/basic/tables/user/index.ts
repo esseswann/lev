394b25258ce1b082a31ba80f4c69b1c79ed07d13
jsus <esseswann@yandex.ru>
2023-08-06 17:56:53 +0300

    Using columns reducer

--- tests/basic/tables/user/index.ts
+++ tests/basic/tables/user/index.ts
@@ -1,4 +1,4 @@
-import { Column, Session, TableDescription, TypedValues, Types, Ydb } from "ydb-sdk";
+import { Column, Session, StructFields, TableDescription, TypedValues, Types, Ydb } from "ydb-sdk";
 import data from './data.json'
 
 const table = new TableDescription()
@@ -10,13 +10,15 @@ export const up = (session: Session, path: string) =>
   session.createTable(path, table)
 
 export const seed = (session: Session, path: string) =>
-  session.bulkUpsert(path, convertRows(data))
+  session.bulkUpsert(path, convertRows(table, data))
 
-const convertRows = (data: unknown[]): Ydb.TypedValue =>
-  TypedValues.list(Types.struct(table.columns.reduce((result, column) => {
-    result[column.name] = column.type
-    return result
-  }, {})), data) as Ydb.TypedValue
+const convertRows = (table: TableDescription, data: unknown[]): Ydb.TypedValue =>
+  TypedValues.list(Types.struct(table.columns.reduce(columnsReducer, {})), data) as Ydb.TypedValue
+
+const columnsReducer = (result: StructFields, { name, type }: Column) => ({
+  ...result,
+  [name]: type
+})
 
 export const down = (session: Session, path: string) =>
   session.describeTable(path)
