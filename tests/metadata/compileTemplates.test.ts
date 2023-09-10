0fbe84441d97d589e20f1fe824afe8fbf53ba3a3
Sergei Popinevskii <gurza000@gmail.com>
2023-09-10 20:14:56 +0300

    rewrite test with jest mock

--- tests/metadata/compileTemplates.test.ts
+++ tests/metadata/compileTemplates.test.ts
@@ -1,11 +1,28 @@
-import { PathLike } from 'fs'
+import fs from 'fs/promises'
 import { getTemplates } from '../../src/metadata/compileTemplates'
 
 describe('getTemplates', () => {
-  it('should return an empty map if the directory does not exist', async () => {
-    const nonExistentPath: PathLike = 'nonExistentPath'
-    const result = await getTemplates(nonExistentPath)
-    expect(result).toBeInstanceOf(Map)
+  beforeAll(() => {
+    jest.mock('fs')
+  })
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('returns an empty map if the path does not exist', async () => {
+    fs.stat = jest.fn().mockRejectedValueOnce(new Error('Path not accessible'))
+
+    const result = await getTemplates('/tmp/metadata/templates')
+    expect(result.size).toBe(0)
+  })
+
+  it('returns an empty map if the path is not a directory', async () => {
+    fs.stat = jest
+      .fn()
+      .mockResolvedValueOnce({ isDirectory: () => false } as any)
+
+    const result = await getTemplates('/tmp/metadata/templates')
     expect(result.size).toBe(0)
   })
 })
